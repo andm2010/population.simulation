819910812ed9c9a02619482cff89e5ea6e82bc69
Andrew Maher <andm2010@gmail.com>
2018-12-06 09:44:28 +0200

    test for input functions added

--- tests/testthat/test_generate_birth_counts.R
+++ tests/testthat/test_generate_birth_counts.R
@@ -1,6 +1,40 @@
+context("generate_birth_counts")
+
 library(testthat)
 library(population.simulation)
 
 test_check("population.simulation")
 
-# test: generate_birth_counts
+
+# define function
+generate_birth_counts <- function(total_births,
+                                  birth_dates, delta){
+
+  birth_rate <- function(birth_dates, delta){
+    times = seq(0, max(birth_dates) - min(birth_dates), delta)
+    birth_rates = seq(times)/sum(times)
+
+    return(birth_rates)
+  }
+
+  birth_counts =  birth_rate(birth_dates, delta) * total_births
+
+  return(birth_counts)
+}
+
+# Call function
+generate_birth_counts(1000, 1984:1990, 1)
+
+# Run expectations
+expect_is(x, "numeric")
+expect_is(x, "factor")
+expect_length(x, 1)
+expect_length(x, 2)
+expect_equivalent(length(x), length(max(birth_dates):min(birth_dates)))
+expect_false(is.array(x))
+expect_true(is.vector(x))
+
+
+
+
+
--- tests/testthat/test_generate_incidence.R
+++ tests/testthat/test_generate_incidence.R
@@ -1,6 +1,56 @@
+context("generate_incidence")
+
 library(testthat)
 library(population.simulation)
 
 test_check("population.simulation")
 
-# test: generate_incidence
+# define function
+generate_incidence <- function(t, age ) {
+  incidence = exp(-(1/age)*t)*0.1
+  return(incidence)
+}
+
+# Call function
+x <- generate_incidence(t=25, age=25 )
+
+# Run expectations
+expect_is(x, "numeric")
+expect_is(x, "factor")
+expect_length(x, 1)
+expect_length(x, 2)
+expect_equivalent(length(x), length(t))
+expect_equivalent(length(x), length(age))
+expect_false(is.array(x))
+expect_true(is.vector(x))
+
+
+
+
+#Or......
+
+generate_incidence <- function(t=25, constant = 0.05, agemin = 0,
+                               agemax = 50,  agepeak= 25,
+                               Imin =0.01,  Ipeak =0.05,
+                               Ifin =0.02){
+
+  incidence = ifelse(t <= agemin, 0,
+                     ifelse(t <= agepeak, Imin + ((Ipeak - Imin)/(agepeak - agemin)) * (t - agemin),
+                            ifelse(t <= agemax, Ipeak + ((Ifin - Ipeak )/(agemax - agepeak)) * (t - agepeak), 0)))
+
+  return(incidence)
+}
+
+y <- generate_incidence()
+
+# Run expectations
+expect_is(x, "numeric")
+expect_is(x, "factor")
+expect_length(x, 1)
+expect_length(x, 2)
+expect_equivalent(length(x), length(t))
+expect_equivalent(length(x), length(age))
+expect_false(is.array(x))
+expect_true(is.vector(x))
+expect_gt(y, Ipeak)
+expect_gt(y, Imin)
